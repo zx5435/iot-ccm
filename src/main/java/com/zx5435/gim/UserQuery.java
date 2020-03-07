ede1afa5c07f5d40b4e4cbc593589e620d62cb61
zx5435 <825407762@qq.com>
2020-03-08 00:20:40 +0800

    asdfasd

--- src/main/java/com/zx5435/gim/UserQuery.java
+++ src/main/java/com/zx5435/gim/UserQuery.java
@@ -6,7 +6,8 @@ import com.zx5435.gim.model.UserDO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class UserQuery implements GraphQLQueryResolver {
@@ -15,7 +16,12 @@ public class UserQuery implements GraphQLQueryResolver {
     UserDAO userDAO;
 
     public UserDO getUserById(Integer id) {
+        long a = System.currentTimeMillis();
         Optional<UserDO> byId = userDAO.findById(id);
+
+        long b = System.currentTimeMillis();
+
+        System.out.println(b - a);
         return byId.orElse(null);
     }
 
